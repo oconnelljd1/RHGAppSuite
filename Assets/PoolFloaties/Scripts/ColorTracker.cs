f714c53376cda9f53d3405856335dc3227a0ecab
James <oconnelljd1@gmail.com>
2025-02-19 14:21:02 -0400

    restructured for asset bundle and set bundle name

---
+++ Assets/PoolFloaties/Scripts/ColorTracker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace PoolFloaties
+{
+    public class ColorTracker : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _display;
+        [SerializeField] private GameObject _minusButton;
+        private int count;
+        
+        void Start()
+        {
+            Reset();
+        }
+        
+        public void Increase()
+        {
+            _minusButton.SetActive(true);
+            count++;
+            UpdateDisplay();
+        }
+
+        public void Decrease()
+        {
+            count--;
+            if(count == 0)
+            {
+                _minusButton.SetActive(false);
+            }
+            UpdateDisplay();
+        }
+
+        public void Reset()
+        {
+            count = 0;
+            _minusButton.SetActive(false);
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            _display.text = count + "";
+        }
+    }
+}
---
+++ Assets/PoolFloaties/Scripts/FloatiesController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PoolFloaties
+{
+    public class FloatiesController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _blackout;
+        [SerializeField] private List<ColorTracker> _colors;
+        [SerializeField] private List<Toggle> _toggles;
+
+        private void Start() {
+            Screen.sleepTimeout  = SleepTimeout.NeverSleep;
+            Screen.orientation = ScreenOrientation.Portrait;
+            
+            _blackout.SetActive(false);
+        }
+
+        public void UpdateColors()
+        {
+            Debug.Log($"<color=orange>Update Colors</color>");
+            for(int i = 0; i < _toggles.Count; i++)
+            {
+                _colors[i].gameObject.SetActive(_toggles[i].isOn);
+            }
+        }
+
+        public void ToggleBlackout()
+        {
+            _blackout.SetActive(!_blackout.activeInHierarchy);
+        }
+
+        public void Reset()
+        {
+            foreach(var toggle in _toggles)
+            {
+                toggle.isOn = true;
+            }
+            foreach (var color in _colors)
+            {
+                color.Reset();
+            }
+        }
+    }
+}
